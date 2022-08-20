5d23a8c5d13076e9c3adda248fdc60dc08dd8000
Jarves020 <47521386+Jarves020@users.noreply.github.com>
2022-08-20 14:49:17 +0200

    Test PID Slew limit 200 Mass 500

--- source/Core/BSP/MHP30/configuration.h
+++ source/Core/BSP/MHP30/configuration.h
@@ -156,7 +156,7 @@
 #define NEEDS_VBUS_PROBE 0
 
 #define HARDWARE_MAX_WATTAGE_X10 650
-#define TIP_THERMAL_MASS         10000 // Test with 20; TODO, needs refinement
+#define TIP_THERMAL_MASS         500 // Test with 20; TODO, needs refinement
 #define TIP_RESISTANCE           60 // x10 ohms, ~6 typical
 #endif
 
