c32ff1a89d489d0c216f9563ba44f7d9991caf1b
Jarves020 <47521386+Jarves020@users.noreply.github.com>
2022-08-20 12:02:43 +0200

    Test different Thermal Mass for PID refinement

--- source/Core/BSP/MHP30/configuration.h
+++ source/Core/BSP/MHP30/configuration.h
@@ -156,7 +156,7 @@
 #define NEEDS_VBUS_PROBE 0
 
 #define HARDWARE_MAX_WATTAGE_X10 650
-#define TIP_THERMAL_MASS         260 // Test with 4x65 TODO, needs refinement
+#define TIP_THERMAL_MASS         260 // Test with 4x65; TODO, needs refinement
 #define TIP_RESISTANCE           60 // x10 ohms, ~6 typical
 #endif
 
